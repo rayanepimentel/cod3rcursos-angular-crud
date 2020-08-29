760a24e24c9fcb44490c76560d86b82a084c65ec
Rayane <radharayg@gmail.com>
2020-08-29 18:50:53 -0300

    Service delete

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -45,4 +45,10 @@ export class ProductService {
     const url = `${this.baseUrl}/${product.id}`
     return this.http.put<Product>(url, product)
   }
+
+  //função excluir produto
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url)
+  }
 }
